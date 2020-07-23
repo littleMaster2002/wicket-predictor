3c8e119f1d5aa9850f225d8045d22135334fc491
littleMaster2002 <48654412+littleMaster2002@users.noreply.github.com>
2020-07-23 23:12:56 +0100

    Add files via upload

---
+++ odis/Wicket Predictor (live).py
@@ -0,0 +1,76 @@
+import pickle
+import requests
+import json
+
+filename = 'model.sav'
+model = pickle.load(open(filename,'rb'))
+print("Please enter the Cricinfo match ID.")
+matchID = int(input())
+balls = []
+rollingSumRuns = 0
+test_x = []
+length = 0
+counter = 0
+while True:
+    length = 0
+    page = requests.get("https://www.espncricinfo.com/matches/engine/match/{}.json".format(matchID))
+    page = page.json()
+    for i in range(len(page["live"]["recent_overs"])):
+        for j in range(len(page["live"]["recent_overs"][i])):
+            length += 1
+            overNumber = int(page["live"]["recent_overs"][i][j]['over_number'])
+            ballNumber = int(page["live"]["recent_overs"][i][j]['ball_number'])
+            if page["live"]["recent_overs"][i][j]['ball'] == '&bull;':
+                balls.append(0)
+                position = len(balls)
+            elif page["live"]["recent_overs"][i][j]['ball'] == 'W':
+                balls.append(0)
+                position = len(balls)
+            else:
+                balls.append(int(page["live"]["recent_overs"][i][j]['ball']))
+                position = len(balls)
+            rollingSumRuns = 0
+            try:
+                if length >= 10:
+                    for k in range(position-10,position):
+                        rollingSumRuns += balls[k]
+            except IndexError:
+                pass
+            if length >= 10:
+                if counter == 1:
+                    test_x.append([rollingSumRuns])
+                if counter > 1:
+                    if overNumber == lastOverNumber and ballNumber > lastBallNumber:
+                        test_x.append([rollingSumRuns])
+                        add = True
+                        predictX = test_x.copy()
+                        predictRolling = predictX[len(predictX)-1][0]
+                        predictRolling += 0
+                        try:
+                            predictRolling -= balls[position-10]
+                        except IndexError:
+                            pass
+                        predictX.append([predictRolling])
+                    if overNumber > lastOverNumber:
+                        test_x.append([rollingSumRuns])
+                        add = True
+                        predictX = test_x.copy()
+                        predictRolling = predictX[len(predictX)-1][0]
+                        predictRolling += 0
+                        try:
+                            predictRolling -= balls[position-10]
+                        except IndexError:
+                            pass
+                        predictX.append([predictRolling])
+    lastOverNumber = overNumber
+    lastBallNumber = ballNumber
+    if len(test_x) > 10:
+        try:
+            probability = model.predict_proba(predictX)
+            if add == True:
+                print(probability[len(probability)-1])
+                add = False
+        except NameError:
+            pass
+    counter += 1
+
---
+++ odis/Wicket Predictor (new).py
@@ -0,0 +1,98 @@
+import yaml
+import glob
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+from imblearn.over_sampling import RandomOverSampler
+import pickle
+import numpy as np
+
+
+data = []
+
+runs = []
+wickets = []
+
+runs10balls = []
+
+wicketCounter = 0
+
+for filename in glob.glob('*.yaml'):
+    wicketCounter = 0
+    with open(filename, 'r') as stream:
+        try:
+            dictionary = yaml.safe_load(stream)
+            dictionary = dictionary["innings"]
+            for i in range(len(dictionary[0]["1st innings"]["deliveries"])):
+                rollingSumRuns = 0
+                key = list(dictionary[0]["1st innings"]["deliveries"][i].keys())[0]
+                if "wicket" in dictionary[0]["1st innings"]["deliveries"][i][key]:
+                    wicketExists = True
+                else:
+                    wicketExists = False
+                if wicketExists == True:
+                    wicketCounter += 1
+                runs.append(dictionary[0]["1st innings"]["deliveries"][i][key]["runs"]["total"])
+                if len(runs) - 10 >= 0:
+                    for k in range(len(runs)-10,len(runs)):
+                        rollingSumRuns += runs[k]
+                else:
+                    for k in range(0,len(runs)):
+                        rollingSumRuns += runs[k]
+                runs10balls.append(rollingSumRuns)
+                if wicketExists == True:
+                    wickets.append(1)
+                else:
+                    wickets.append(0)
+            if [1] in dictionary:
+                for j in range(len(dictionary[1]["2nd innings"]["deliveries"])):
+                    rollingSumRuns = 0
+                    key = list(dictionary[1]["2nd innings"]["deliveries"][j].keys())[0]
+                    if "wicket" in dictionary[1]["2nd innings"]["deliveries"][j][key]:
+                        wicketExists = True
+                    else:
+                        wicketExists = False
+                    if wicketExists == True:
+                        wicketCounter += 1
+                    runs.append(dictionary[1]["2nd innings"]["deliveries"][j][key]["runs"]["total"])
+                    if len(runs) - 10 >= 0:
+                        for k in range(len(runs)-10,len(runs)):
+                            rollingSumRuns += runs[k]
+                    else:
+                        for k in range(0,len(runs)):
+                            rollingSumRuns += runs[k]
+                    runs10balls.append(rollingSumRuns)
+                    if wicketExists == True:
+                        wickets.append(1)
+                    else:
+                        wickets.append(0)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+RUNS10BALLS = []
+WICKETS = []
+
+for i in range(len(runs10balls)):
+    RUNS10BALLS.append([runs10balls[i]])
+for i in range(len(wickets)):
+    WICKETS.append([wickets[i]]) 
+
+train_x,test_x,train_y,test_y = train_test_split(RUNS10BALLS, WICKETS, random_state = 10)
+
+
+ros = RandomOverSampler(sampling_strategy=0.8,random_state=0)
+xR,yR = ros.fit_resample(train_x,np.ravel(train_y))
+
+dtree_model = DecisionTreeClassifier(max_depth=10).fit(xR,yR)
+dtree_predictions = dtree_model.predict(test_x)
+
+accuracyDT = dtree_model.score(test_x,test_y)
+print(accuracyDT)
+
+cmDT = confusion_matrix(test_y,dtree_predictions)
+print(cmDT)
+
+filename = 'model.sav'
+pickle.dump(dtree_model,open(filename,'wb'))
+
